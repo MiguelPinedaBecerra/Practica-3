7a81dd96c3dce1c056bdabee7bb55d8519f8b8e3
MiguelPinedaBecerra <23308060610467@cetis61.edu.mx>
2026-02-25 11:44:54 -0500

    asdaw

--- Propina/main.py
+++ Propina/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from propina!")
+import flet as ft
 
-
-if __name__ == "__main__":
-    main()
+def main(page: ft.page):
+    page.title ="Propinas"
+    page.vertical_alignment = ft.MainAxisAligmnement.CENTER
+    
+    monto = ft.TextField(label = "Escribe el monto a pagar", width = 200)
+    txt_valor = ft.Text(value = "5%", size = 25)
+    
+    def calcular(e):
+        txt_valor.value =str(int(slider.value)) + '%'
+        if monto.value != "":
+            propina = int(monto.value) * slider.value / 100
+            total = int(monto.value) + propina
+            txt_propina.value = "La propona e: " + str(round(propina,2))
+            txt_total.value = "El total a pagar es: " + str(round(total, 2))
+            page.update()
+            
+    txt_propina = ft.Text("La propina es: ", size = 25)
+    txt_total = ft.Text("El total a pagar es: ", size = 25)
+        
+    slider = ft.Sñider(min =5, max = 25, value = 5, on_change = clacular)
+    page.add(txt_valor, monto, slider, txt_propina, txt_total)
+    
+    
+            
+ft.app(target = main)
